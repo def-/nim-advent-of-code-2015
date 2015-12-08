c9f64ade96af1e20d316c9c3aed429a47c32add3
def <dennis@felsin9.de>
2015-12-08 22:07:02 +0100

    Day 1

---
+++ day1.nim
@@ -0,0 +1,32 @@
+import strutils
+
+# Part 1
+
+proc concise(file: File): int =
+  let input = file.readAll
+  input.count('(') - input.count(')')
+
+proc efficient(file: File): int =
+  while not endOfFile file:
+    case readChar file
+    of '(': inc result
+    of ')': dec result
+    else: discard
+
+#echo stdin.concise
+#echo open("day1.input").concise
+#echo open("day1.input").efficient
+
+# Part 2
+
+proc part2(file: File): tuple[total, pos: int64] =
+  while not endOfFile file:
+    case readChar file
+    of '(': inc result.total
+    of ')': dec result.total
+    else: discard
+
+    if result.pos == 0 and result.total == -1:
+      result.pos = getFilePos file
+
+echo open("day1.input").part2 # (total: 232, pos: 1783)
