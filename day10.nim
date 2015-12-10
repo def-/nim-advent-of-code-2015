453fbc1d43fc2246d70ae72bb659eb19810696ad
def <dennis@felsin9.de>
2015-12-10 06:42:56 +0100

    Day 10 cleanup

--- day10.nim
+++ day10.nim
@@ -1,20 +1,47 @@
 import strutils, sequtils, tables, algorithm
 
-var x = "1113122113"
+proc part1: int =
+  var x = "1113122113"
 
-for j in 1..50:
-  var prev = x[0]
-  var y = ""
-  var count = 0
-  for i, c in x:
-    if c != prev:
-      y.add($count)
-      y.add x[i-1]
+  for round in 1..40:
+    var
+      y = ""
+      prev = x[0]
       count = 0
-    inc count
-    prev = c
-  y.add($count)
-  y.add x[^1]
-  x = y
 
-echo x.len
+    for i, c in x:
+      if c != prev:
+        y.add($count & x[i-1])
+        count = 0
+      inc count
+      prev = c
+
+    y.add($count & x[^1])
+    x = y
+
+  result = x.len
+
+echo "Part 1: ", part1()
+
+proc part2: int =
+  var x = "1113122113"
+
+  for round in 1..50:
+    var
+      y = ""
+      prev = x[0]
+      count = 0
+
+    for i, c in x:
+      if c != prev:
+        y.add($count & x[i-1])
+        count = 0
+      inc count
+      prev = c
+
+    y.add($count & x[^1])
+    x = y
+
+  result = x.len
+
+echo "Part 2: ", part2()
